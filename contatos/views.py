9b284881bb84a31183a9d19b1390ad48b4fde905
Davi-Snt <britodavi122@gmail.com>
2021-12-25 18:59:21 -0300

    Ajustando o método de ordenação e filtração dos valores exibidos.

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.http import Http404
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True,
+    )
     paginator = Paginator(contatos, 10)
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
@@ -16,6 +19,10 @@ def index(request):
 
 def details(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+    
+    if not contato.mostrar:
+        raise Http404()
+    
     return render(request, 'contatos/detalhes.html', {
         'contato': contato
     })
